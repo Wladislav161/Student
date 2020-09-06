f64423294fa017851b697efb6ac7cba1dbd18f83
Wladislav161 <corvinm@mail.ru>
2020-09-06 12:18:05 +0300

    Добавил расчет среднего балла

--- ClassStudent.h
+++ ClassStudent.h
@@ -1,41 +1,86 @@
 #pragma once
+#pragma once
 #include <string>
 
 using namespace std;
 
 struct Date
 {
-    int year;
-    int month;
-    int day;
+	int year;
+	int month;
+	int day;
 };
 
 class Student
 {
 public:
-    string lastName; //Фамилия
-    string firstName; //Имя
-    Date dateBirth; //Дата рождения
-    string faculty; //Факультет
-    string group; //Группа
-    int stage; //Курс
-    bool study; //Учится?
-    void SetAge(int age); //Установка занчения возраста
-    int GetAge(); //Получение значения возраста
+	string lastName;
+	string firstName;
+	Date dateBirth;
+	string faculty;
+	string group;
+	int stage;
+	bool study;
+	int ocenka[5];
+	void SetAge(int age);
+	int GetAge();
+	double SredniyBall();
+
 private:
-    int age; //Возраст
+	double srednyBall;
+	int age;
+	void SredBall();
+	void UspivStudent();
 };
 
 void Student::SetAge(int age)
 {
-    if (age <= 10  || age > 80) {
-        throw -1;
-    } else {
-        this->age = age;
-    }
+	if (age <= 10 || age > 80) {
+		throw - 1;
+	}
+	else {
+		this->age = age;
+	}
 }
 
 int Student::GetAge()
 {
-    return age;
+	return age;
+}
+
+void Student::SredBall()
+{
+	double sum = 0;
+	for (int i = 0; i < 5; ++i)
+	{
+		sum += ocenka[i];
+	}
+	srednyBall = sum / 5;
+}
+
+double Student::SredniyBall()
+{
+	UspivStudent();
+	SredBall();
+	return srednyBall;
+}
+
+void Student::UspivStudent() {
+	int studentOcenka;
+
+	for (int i = 0; i < sizeof(ocenka) / sizeof(int);)
+	{
+		cout << "Enter " << i + 1 << " student ocenka: "; cin >> studentOcenka;
+
+		if (studentOcenka < 1 || studentOcenka>5)
+		{
+			cout << "Invalid ocenka." << endl;
+		}
+
+		else
+		{
+			ocenka[i] = studentOcenka;
+			++i;
+		}
+	}
 }
